25d7dfb52d380afc647af82cc609a41f95d76c7d
Mariam Hussein <mariamh99@gmail.com>
2025-04-02 14:05:25 +0100

    Create script1.c

---
+++ script1.c
@@ -0,0 +1,25 @@
+//Final Project - Task 1
+//Ali Abdulla A00786, Mariam Hussein A00039
+//American University of Bahrain
+//COSC372L - Operating Systems Lab
+//December 20, 2022
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_SIZE 1024
+
+int main(void)
+{
+    // Allocate memory for input
+    char input[MAX_INPUT_SIZE];
+    
+    // Prompt user to input data
+    printf("Input data: ");
+    
+    // Read input using fgets
+    if (fgets(input, MAX_INPUT_SIZE, stdin) != NULL) {
+        printf("The inputted data is: %s", input);
+    }
+    
+    return 0;
+}
